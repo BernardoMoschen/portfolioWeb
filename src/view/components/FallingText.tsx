782639fcdc87b72a8774fe8c1088439486302bb3
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-11-21 20:20:26 -0300

    refactor: remove scale from fallingText

--- src/view/components/FallingText.tsx
+++ src/view/components/FallingText.tsx
@@ -17,14 +17,12 @@ export default function FallingText({
     <Component
       initial={{
         opacity: 0,
-        scale: 0.75,
         y: -50,
       }}
       viewport={{ once: true }}
       whileInView={{
         y: 0,
         opacity: 1,
-        scale: 1,
         transition: {
           duration: 1,
         },
