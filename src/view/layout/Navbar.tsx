08ac9199411fb45622023c06a86d56c86e62f18c
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-11-21 20:05:44 -0300

    feat: hero initial

--- src/view/layout/Navbar.tsx
+++ src/view/layout/Navbar.tsx
@@ -6,29 +6,29 @@ export default function Navbar() {
   const background = useTransform(
     scrollY,
     [0, 100],
-    ['rgba(0, 183, 255, 0)', 'rgba(0, 183, 255, 1)']
+    ['rgba(0, 183, 255, 0)', 'rgba(60, 0, 0, 0.9)']
   );
   const height = useTransform(scrollY, [0, 100], [120, 60]);
 
   return (
     <motion.div
-      className='w-full h-15 fixed top-0 right-0 left-0 z-15 bg-[rgba(0,183,255,0)] text-[rgba(21,21,21,1)] flex flex-row items-center justify-around px-6 font-bold text-xs tracking-[0.0200rem]'
+      className='w-full h-15 fixed top-0 right-0 left-0 z-15 text-white flex flex-row items-center justify-around px-6 font-bold text-xs tracking-[0.0200rem]'
       style={{
         background,
         height,
       }}
     >
-      <div className='text-2xl bg-amber-950 w-auto h-auto rounded-[30px] mr-auto text-white text-center'>
+      <div className='text-2xl  w-auto h-auto rounded-[30px] mr-autotext-center'>
         Bernardo Moschen
       </div>
       <ul className='flex items-center w-auto'>
-        <li className='text-2xl my-0 mx-4 bg-amber-500 w-auto h-auto rounded-[5px] '>
+        <li className='text-2xl my-0 mx-4  w-auto h-auto rounded-[5px] '>
           Work
         </li>
-        <li className='text-2xl my-0 mx-4 bg-amber-500 w-auto h-auto rounded-[5px]'>
+        <li className='text-2xl my-0 mx-4  w-auto h-auto rounded-[5px]'>
           About
         </li>
-        <li className='text-2xl my-0 mx-4 bg-amber-500 w-auto h-auto rounded-[5px]'>
+        <li className='text-2xl my-0 mx-4  w-auto h-auto rounded-[5px]'>
           Contact
         </li>
       </ul>
--- src/view/pages/Home/components/Hero.tsx
+++ src/view/pages/Home/components/Hero.tsx
@@ -1,17 +1,17 @@
 export default function Hero() {
   return (
-    <div className='w-full bg-lime-400'>
-      <p className='font-semibold text-xl text-black text-center p-8'>
+    <div className='w-full text-darkRed-200'>
+      <p className='font-semibold text-xl  text-center p-8'>
         Full Stack Developer based in Brasil
       </p>
-      <div className='flex justify-between'>
-        <p className='font-semibold text-4xl text-black text-start p-2 '>
-          Creative
-          <br />
-          Developer
-        </p>
+      <p className='w-full px-5 md:px-20 pb-20 font-semibold text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl justify-start'>
+        Creative
+        <br />
+        Developer
+      </p>
+      <div className='flex justify-end w-full px-5 md:px-20 mb-20 font-semibold relative'>
         <img
-          className='object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]'
+          className='block w-full md:w-1/2'
           src='https://images.unsplash.com/photo-1566545455366-bcae28fd3929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1956&q=80'
         />
       </div>
