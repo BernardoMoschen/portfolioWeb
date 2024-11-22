a28a602f4cdbe40198e48db5597bfb627eb22ff3
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-11-21 22:09:31 -0300

    refactor: adds blur to layout

--- src/view/layout/Layout.tsx
+++ src/view/layout/Layout.tsx
@@ -1,13 +1,25 @@
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
+import { motion } from 'motion/react';
 
 export default function Layout() {
   return (
-    <>
+    <motion.div
+      initial={{
+        filter: 'blur(4px)',
+      }}
+      viewport={{ once: true }}
+      whileInView={{
+        filter: 'blur(0px)',
+        transition: {
+          duration: 0.75,
+        },
+      }}
+    >
       <Navbar />
       <div className='pt-[120px] h-full w-full'>
         <Outlet />
       </div>
-    </>
+    </motion.div>
   );
 }
