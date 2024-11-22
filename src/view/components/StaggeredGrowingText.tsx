81149e5f822edbd429a122be227d64d49451fc8f
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-11-21 21:00:17 -0300

    feat: staggeredGrowText

---
+++ src/view/components/StaggeredGrowingText.tsx
@@ -0,0 +1,69 @@
+import { motion, Variants } from 'framer-motion';
+
+interface Props {
+  children: string;
+  childrenDelay?: number;
+  childrenStagger?: number;
+  className?: string;
+}
+
+const childVariants: Variants = {
+  hidden: {
+    y: '+100%',
+    transition: {
+      type: 'spring',
+      damping: 0,
+      stiffness: 0,
+    },
+  },
+  visible: {
+    y: 0,
+    transition: {
+      type: 'spring',
+      damping: 50,
+      stiffness: 200,
+    },
+  },
+};
+
+export const StaggeredGrowingText = ({
+  children,
+  childrenDelay = 0,
+  childrenStagger = 0.055,
+}: Props) => {
+  const containerVariants: Variants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: {
+        ease: 'easeOut',
+        staggerChildren: childrenStagger,
+        delayChildren: i * childrenDelay,
+      },
+    }),
+  };
+
+  return (
+    <motion.div
+      className='flex overflow-hidden'
+      variants={containerVariants}
+      viewport={{ once: true }}
+      whileInView='visible'
+      initial='hidden'
+    >
+      <div>
+        {children.split('').map((lt, i) => (
+          <motion.span
+            key={i}
+            variants={childVariants}
+            className='inline-block'
+          >
+            {lt === ' ' ? '\u00A0' : lt}
+          </motion.span>
+        ))}
+      </div>
+    </motion.div>
+  );
+};
