136237b05352f154f4410e7c5f5c197204506f65
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-11-21 23:14:08 -0300

    fix: unused variable

--- src/view/pages/Home/index.tsx
+++ src/view/pages/Home/index.tsx
@@ -2,14 +2,13 @@ import Hero from './components/Hero';
 
 export function Home() {
   const boxCount = Array.from(Array(100).keys());
-  console.log(boxCount.length);
 
   return (
     <>
       <div className='h-full'>
         <Hero />
         <>
-          {boxCount.map((box, index) => (
+          {boxCount.map((_box, index) => (
             <div
               className='w-[30%] h-[100px] m-[1.5%] rounded-[10px] bg-cyan-200 inline-block'
               key={index}
