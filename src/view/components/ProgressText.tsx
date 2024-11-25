b4ef10a7592de0ff87f9e1c451d00c408c563f44
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-11-25 11:24:15 -0300

    feat: progressText

--- src/view/components/ProgressText.tsx
+++ src/view/components/ProgressText.tsx
@@ -1,24 +1,59 @@
-import { useScroll, motion } from 'motion/react';
-import { ReactNode, useEffect, useRef } from 'react';
+import { useScroll, motion, MotionValue, useTransform } from 'motion/react';
+import { ReactNode, useRef } from 'react';
 
 interface Props {
-  children: ReactNode;
+  children: string;
 }
 
 export default function ProgressText({ children }: Props) {
-  const pElement = useRef<HTMLParagraphElement>(null);
+  return <ProgressParagraph>{children}</ProgressParagraph>;
+}
+
+interface ProgressParagraph {
+  children: string;
+}
+
+function ProgressParagraph({ children }: ProgressParagraph) {
+  const words = children.split(' ');
+
+  const spanElement = useRef<HTMLParagraphElement>(null);
   const { scrollYProgress } = useScroll({
-    target: pElement,
+    target: spanElement,
     offset: ['start 0.9', 'start 0.25'],
   });
 
-  useEffect(() => {
-    scrollYProgress.on('change', (e) => console.log(e));
-  }, []);
-
   return (
-    <motion.p ref={pElement} style={{ opacity: scrollYProgress }}>
-      {children}
+    <motion.p
+      className='flex flex-wrap p-[40px]'
+      ref={spanElement}
+      style={{ opacity: scrollYProgress }}
+    >
+      {words.map((word, i) => {
+        const start = i / words.length;
+        const end = start + 1 / words.length;
+
+        return (
+          <ProgressWord key={i} range={[start, end]} progress={scrollYProgress}>
+            {word}
+          </ProgressWord>
+        );
+      })}
     </motion.p>
+  );
+}
+
+interface ProgressWord {
+  children: ReactNode;
+  range: [number, number];
+  progress: MotionValue<number>;
+}
+
+function ProgressWord({ children, range, progress }: ProgressWord) {
+  const opacity = useTransform(progress, range, [0, 1]);
+  return (
+    <span className='mr-4 mt-4 relative'>
+      <span className='absolute opacity-[0.3]'>{children}</span>
+      <motion.span style={{ opacity }}>{children}</motion.span>
+    </span>
   );
 }
