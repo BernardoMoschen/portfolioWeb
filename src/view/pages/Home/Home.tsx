0c92e2c6627b500c4e6a5201efed5a6546bdf66f
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-11-27 16:34:19 -0300

    feat: responsive hero

--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -16,15 +16,17 @@ import { Container } from '@/view/components';
 // );
 
 export const Home = () => (
-  <div className='grid grid-cols-12 grid-rows-2'>
-    <Container className='col-span-full md:col-span-9 min-h-[100%] min-w-[100%]'>
-      <h1 className='text-3xl md:text-2xl lg:text-2xl text-white'>
-        Hi, i am <span className='text-red-600'>Bernardo</span>
-      </h1>
-      <h3 className='text-2xl md:text-4xl lg:text-8xl text-white'>
-        FullStack Developer
-      </h3>
+  <div className='grid grid-cols-12 grid-rows-2 '>
+    <Container className='col-span-full md:col-span-8 min-h-[100%] min-w-[100%]'>
+      <div className='p-[5%]'>
+        <h1 className='text-3xl md:text-2xl lg:text-2xl text-white'>
+          Hi, i am <span className='text-red-600'>Bernardo</span>
+        </h1>
+        <h3 className='text-2xl md:text-4xl lg:text-8xl text-white'>
+          FullStack Developer
+        </h3>
+      </div>
     </Container>
-    <Container className='mt-[15%] min-h-[100%] min-w-[100%] col-start-6 col-span-full md:col-span-1 md:col-start-11' />
+    <Container className='mt-[25%] min-h-[100%] min-w-[100%] col-start-6 col-span-full md:col-span-2 md:col-start-10' />
   </div>
 );
