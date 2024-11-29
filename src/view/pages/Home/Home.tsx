d11b699447697797972aafb28cba1bbea66398d8
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-11-28 21:23:29 -0300

    refactor: divide hero

--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import { Container } from '@/view/components';
 
-export const Home = () => (
+
+export const Hero = () => (
   <div className='relative grid grid-cols-12 gap-4 grid-rows-'>
     <Container
       className='
         col-span-full
-        md:col-span-8
+        md:col-span-12
         row-span-1
         md:row-span-full
         min-h-[100%]
@@ -20,34 +21,11 @@ export const Home = () => (
         </h3>
       </div>
     </Container>
-
-
-    {/* <Container
-      className={`
-        hidden
-        md:grid
-        absolute
-        col-span-4
-        md:col-span-5
-        row-start-1
-        row-span-4
-        md:row-span-4
-        col-start-8
-        md:col-start-7
-        h-[80%]
-        w-[80%]
-        self-center
-      `}
-    >
-      <img className='h-full w-full object-cover rounded-[30px]' src={myself} />
-    </Container>
-    <Container
-      className='hidden
-        md:grid col-span-7 col-start-6 md:col-span-2 md:col-start-10
-        row-span-3
-        md:mt-[15px]
-        md:mb-[10px]
-    min-h-[100%] min-w-[100%]'
-    /> */}
   </div>
+);
+
+export const Home = () => (
+  <>
+    <Hero />
+  </>
 );
