4faadec26080802841d79ace3b84e782d3700c5f
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-12-11 10:45:33 -0300

    chore: cleanup project

--- src/view/layout/index.tsx
+++ src/view/layout/index.tsx
@@ -1,27 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import NavbarBlur from './NavbarBlur';
-// import { AuroraBackground } from '../components/AuroraHero';
 
 export default function Layout() {
   return (
-    // <div className='h-full w-full  static overflow-visible '>
-    // <NavbarBlur />
-    <Outlet />
-    // </div>
+    <div className='h-full w-full static overflow-visible'>
+      <NavbarBlur />
+      <Outlet />
+    </div>
   );
 }
- 
-    /* <motion.div
-            className='bg-tron-background text-tron-beige relative'
-            initial={{
-              filter: 'blur(4px)',
-            }}
-            viewport={{ once: true }}
-            whileInView={{
-              filter: 'blur(0px)',
-              transition: {
-                duration: 0.75,
-              },
-            }}
-          > */
-  
